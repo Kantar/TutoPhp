00ecbe7d12b33b8a631e07d5466c240e75c32b7a
kantar <guillaumelgrs@gmail.com>
2018-08-15 17:09:22 +0200

    modif include codephp

--- index.php
+++ index.php
@@ -34,7 +34,24 @@
 
     </style>
     <?php
-        include("./codephp.php");
+        //include("./codephp.php");
+    $title;
+    $link;
+
+    if(!isset($_GET['title'])){
+        $title = $_GET['title'];
+        var_dump($title);
+        switch ($title){
+            case Tuto1:
+                $link = '"./Tuto1/bonjour.php"';
+                break;
+            default:
+                $link = "codephp : title non prévus";
+                break;
+        }
+    }else{
+        $title ='Accueil';
+    }
     ?>
 </head>
 <body>
